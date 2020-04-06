ef5da64c90a446b179dbc5e11e9a1447e318d62f
John McIntyre <john.mcintyre84@gmail.com>
2020-04-06 18:19:24 +0100

    Session Lobby for Host-Client and Dedicated server model. Team Spawn points Cleaned Start of round

--- Source/GameJam/CodePlayerController.h
+++ Source/GameJam/CodePlayerController.h
@@ -3,10 +3,25 @@
 #pragma once
 
 #include "GameJamCharacter.h"
-
+#include "Engine/DataTable.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "CodePlayerController.generated.h"
+
+
+USTRUCT(BlueprintType)
+struct FTeamDeptData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Office War")
+	TArray<FName> Characters;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Office War")
+	TMap<FName, float> PropertyMods;
+
+};
+
 
 /**
  * 
@@ -23,12 +38,16 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Office War")
 	int CurrentChar;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Office War")
+	FName TeamDept;
 
 public:
 
 	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Office War")
 	void  NextCharacter();
 
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Office War")
+	void  LockAndLoad();
 
 
 };
